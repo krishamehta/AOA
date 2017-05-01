e4edabace95a9db14993991e9497f00bc3d15d38
Krisha Mehta <kkoolkrisha@gmail.com>
2017-05-01 23:56:26 +0530

    Update sumofsubs.c

--- sumofsubs.c
+++ sumofsubs.c
@@ -1,52 +1,39 @@
-#include <stdio.h>
-
-int a[10],x[10], m, n;
-
-sumSub(int sum, int index , int rem_sum){
-
-  int i;
-
-  x[index] = 1;
-
-  if((sum + a[index]) == m){
-    printf("Solution exists:\n");
-
-    for (int i = 1; i <=n ; ++i)
-    {
-      if (x[i] = 1)
-      {
-        printf("%d\n",a[i] );
-      }
-    }
-      printf("\n");
-    }
-
-    else if ((sum + a[index] + a[index+1] < = m))
-    {
-      
-    }
-  
+#include<stdio.h>
+int a[10],x[10],n,m;
+void sumOfSubset(int sum, int index, int rem_sum)
+{
+ int i;
+ x[index]=1;
+ if((sum + a[index])==m)
+ {
+ printf("Solution exists: ");
+ for(i=1;i<=n;i++)
+ {
+ if(x[i]==1)
+ printf(" %d",a[i]);
+ }
+ printf("\n");
+ }
+ else if((sum + a[index] + a[index+1])<=m)
+ sumOfSubset(sum + a[index], index+1, rem_sum - a[index]);
+ if((sum + rem_sum - a[index])>=m && (sum + a[index+1] <=m))
+ {
+ x[index]=0;
+ sumOfSubset(sum, index+1, rem_sum-a[index]);
+ }
 }
-
-void main(){
-
-  int i , rem_sum = 0;
-
-  printf("Enter the number of elements:\n");
-  scanf("%d",&n);
-
-  printf("Enter values in asc order:\n");
-
-  for (int i = 1; i <= n; i++)
-  {
-    scanf("%d",&a[i]);
-    rem_sum = rem_sum + a[i];
-  }
-
-  printf("Enter the sum required:\n");
-  scanf("%d",&m);
-
-  sumSub(0,1,rem_sum);
-
+void main()
+{
+ int i, rem_sum=0;
+ printf("Enter the number of elements: ");
+scanf("%d",&n);
+printf("Enter values in ascending order: ");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+rem_sum = rem_sum + a[i];
 }
-
+printf("Enter the sum required: ");
+scanf("%d",&m);
+sumOfSubset(0,1,rem_sum);
+}
